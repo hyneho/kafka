9e3ba3e0f5a88b6d5c4af7fcb0e9aa563366c861
Lucas Brutschy <lbrutschy@confluent.io>
2024-05-16 22:27:46 +0200

    Resolve merge conflict from 11/25 rebase - Specify AsyncKafkaConsumer interface with Streams for the Rebalance PoC
    Extend internal AsyncKafkaConsumer interface to exchange rebalance information with Streams.
    
    See https://github.com/lucasbru/kafka/pull/9

--- clients/src/test/java/org/apache/kafka/clients/consumer/internals/AsyncKafkaConsumerTest.java
+++ clients/src/test/java/org/apache/kafka/clients/consumer/internals/AsyncKafkaConsumerTest.java
@@ -218,7 +218,8 @@ public class AsyncKafkaConsumerTest {
             a -> backgroundEventReaper,
             (a, b, c, d, e, f, g) -> fetchCollector,
             (a, b, c, d) -> metadata,
-            backgroundEventQueue
+            backgroundEventQueue,
+            Optional.empty()
         );
     }
 
@@ -1320,7 +1321,8 @@ public class AsyncKafkaConsumerTest {
             any(),
             any(),
             any(),
-            applicationThreadMemberStateListener.capture()
+            applicationThreadMemberStateListener.capture(),
+            any()
         ));
         return applicationThreadMemberStateListener.getValue();
     }
