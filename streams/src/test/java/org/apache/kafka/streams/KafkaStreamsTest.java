2a14e5b514fc4bd73d9eb830309e3845dac8d1b7
Nick Telford <nick.telford@gmail.com>
2024-08-27 14:02:03 +0100

    Fix StateDirectory ignores dynamically added threads
    During a rebalance, we check if the current thread is the "last" thread,
    to determine when to close out unassigned Tasks. We were failing to
    account for threads added dynamically via
    `KafkaStreams#addStreamThread()`. This could lead to us closing Tasks
    prematurely.

--- streams/src/test/java/org/apache/kafka/streams/KafkaStreamsTest.java
+++ streams/src/test/java/org/apache/kafka/streams/KafkaStreamsTest.java
@@ -1795,7 +1795,7 @@ public class KafkaStreamsTest {
         try (MockedConstruction<StateDirectory> stateDirectoryMockedConstruction = mockConstruction(StateDirectory.class,
             (mock, context) -> {
                 when(mock.initializeProcessId()).thenReturn(UUID.randomUUID());
-                assertEquals(4, context.arguments().size());
+                assertEquals(5, context.arguments().size());
                 assertEquals(shouldFilesExist, context.arguments().get(2));
             })) {
 
