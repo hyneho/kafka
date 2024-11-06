a6e9cb9c0cefe76172cb406fea0c455ee41156a0
Andrew Schofield <aschofield@confluent.io>
2024-11-06 17:53:53 +0000

    Checkstyle

--- clients/src/main/java/org/apache/kafka/clients/admin/internals/DescribeShareGroupsHandler.java
+++ clients/src/main/java/org/apache/kafka/clients/admin/internals/DescribeShareGroupsHandler.java
@@ -37,7 +37,6 @@ import org.slf4j.Logger;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
