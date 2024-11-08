0e9f227ff310429610f2690bcf3c19a93be0c00c
David Arthur <mumrah@gmail.com>
2024-11-08 14:15:20 -0500

    split the two gradle test invocations

--- test-common/src/main/java/org/apache/kafka/common/test/junit/QuarantinedExecutionCondition.java
+++ test-common/src/main/java/org/apache/kafka/common/test/junit/QuarantinedExecutionCondition.java
@@ -24,6 +24,9 @@ import org.junit.jupiter.api.extension.ExtensionContext;
 import java.lang.reflect.Method;
 import java.util.Optional;
 
+/**
+ * test: run any test without "quarantined" tag and that is not auto-quarantined
+ */
 public class QuarantinedExecutionCondition implements ExecutionCondition {
 
     private final QuarantinedTestSelector selector;
@@ -34,18 +37,45 @@ public class QuarantinedExecutionCondition implements ExecutionCondition {
 
     @Override
     public ConditionEvaluationResult evaluateExecutionCondition(ExtensionContext extensionContext) {
+        boolean runQuarantined = System.getProperty("kafka.test.run.quarantined", "false")
+            .equalsIgnoreCase("true");
+
         Optional<Method> methodOpt = extensionContext.getTestMethod();
         if (methodOpt.isPresent()) {
             Class<?> clazz = extensionContext.getRequiredTestClass();
+
+            // Check if test is quarantined
             Optional<String> reason = selector.selectQuarantined(
                 clazz.getCanonicalName(), methodOpt.get().getName());
+
+            // If the  only run if we've been given "kafka.test.run.quarantined=true"
             if (reason.isPresent()) {
-                return ConditionEvaluationResult.disabled("quarantined", reason.get());
+                if (runQuarantined) {
+                    return ConditionEvaluationResult.enabled("run quarantined test");
+                } else {
+                    return ConditionEvaluationResult.disabled("skip quarantined test", reason.get());
+                }
+            }
+
+            // Only include "flaky" tag if given "kafka.test.run.quarantined=true", otherwise skip it
+            boolean isFlaky = extensionContext.getTags().contains("flaky");
+            if (runQuarantined) {
+                if (isFlaky) {
+                    return ConditionEvaluationResult.enabled("run flaky test");
+                } else {
+                    return ConditionEvaluationResult.disabled("skip non-flaky test");
+                }
             } else {
-                return ConditionEvaluationResult.enabled("not quarantined");
+                if (isFlaky) {
+                    return ConditionEvaluationResult.disabled("skip flaky test");
+                } else {
+                    // Base case
+                    return ConditionEvaluationResult.enabled(null);
+                }
             }
         } else {
-            return ConditionEvaluationResult.enabled("not quarantined");
+            // No method. Just return
+            return ConditionEvaluationResult.enabled(null);
         }
     }
 }
