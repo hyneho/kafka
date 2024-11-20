83fc619267d54dc1e2c319ce4e96a204c5551996
m1a2st <s7133700@gmail.com>
2024-11-21 00:18:52 +0800

    revert change

--- clients/src/main/java/org/apache/kafka/clients/consumer/internals/CoordinatorRequestManager.java
+++ clients/src/main/java/org/apache/kafka/clients/consumer/internals/CoordinatorRequestManager.java
@@ -34,7 +34,6 @@ import org.slf4j.Logger;
 
 import java.util.Objects;
 import java.util.Optional;
-import java.util.concurrent.CompletableFuture;
 
 import static org.apache.kafka.clients.consumer.internals.NetworkClientDelegate.PollResult.EMPTY;
 
@@ -61,7 +60,6 @@ public class CoordinatorRequestManager implements RequestManager {
     private long timeMarkedUnknownMs = -1L; // starting logging a warning only after unable to connect for a while
     private long totalDisconnectedMin = 0;
     private Node coordinator;
-    private CompletableFuture<RuntimeException> metadataError = new CompletableFuture<>();
 
     public CoordinatorRequestManager(
         final LogContext logContext,
@@ -190,8 +188,6 @@ public class CoordinatorRequestManager implements RequestManager {
         if (exception == Errors.GROUP_AUTHORIZATION_FAILED.exception()) {
             log.debug("FindCoordinator request failed due to authorization error {}", exception.getMessage());
             KafkaException groupAuthorizationException = GroupAuthorizationException.forGroupId(this.groupId);
-            metadataError.completeExceptionally(groupAuthorizationException);
-            metadataError = metadataError.newIncompleteFuture();
             backgroundEventHandler.add(new ErrorEvent(groupAuthorizationException));
             return;
         }
@@ -237,7 +233,4 @@ public class CoordinatorRequestManager implements RequestManager {
         return Optional.ofNullable(this.coordinator);
     }
     
-    public CompletableFuture<RuntimeException> metadataError() {
-        return metadataError;
-    }
 }
