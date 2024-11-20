00cc9a06d109ef58c9d6fa35d827206aa149a13e
m1a2st <s7133700@gmail.com>
2024-11-21 00:36:57 +0800

    revert change

--- clients/src/main/java/org/apache/kafka/clients/consumer/internals/CommitRequestManager.java
+++ clients/src/main/java/org/apache/kafka/clients/consumer/internals/CommitRequestManager.java
@@ -415,7 +415,8 @@ public class CommitRequestManager implements RequestManager, MemberStateListener
      *                   an expected retriable error.
      * @return Future that will complete when a successful response
      */
-    public CompletableFuture<Map<TopicPartition, OffsetAndMetadata>> commitSync(final Optional<Map<TopicPartition, OffsetAndMetadata>> offsets, final long deadlineMs) {
+    public CompletableFuture<Map<TopicPartition, OffsetAndMetadata>> commitSync(final Optional<Map<TopicPartition, OffsetAndMetadata>> offsets, 
+                                                                                final long deadlineMs) {
         Map<TopicPartition, OffsetAndMetadata> commitOffsets = offsets.orElseGet(subscriptions::allConsumed);
         if (commitOffsets.isEmpty()) {
             return CompletableFuture.completedFuture(Map.of());
