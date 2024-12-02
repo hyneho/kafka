e617b224ffa5111248217d98dbe9dcfba12d596f
Kuan-Po Tseng <brandboat@gmail.com>
2024-12-03 00:54:04 +0800

    Address comments

--- core/src/test/scala/unit/kafka/server/TxnOffsetCommitRequestTest.scala
+++ core/src/test/scala/unit/kafka/server/TxnOffsetCommitRequestTest.scala
@@ -19,12 +19,13 @@ package kafka.server
 import org.apache.kafka.common.test.api.{ClusterConfigProperty, ClusterInstance, ClusterTest, ClusterTestDefaults, ClusterTestExtensions, Type}
 import kafka.utils.TestUtils
 import org.apache.kafka.common.TopicPartition
+import org.apache.kafka.common.errors.UnsupportedVersionException
 import org.apache.kafka.common.protocol.{ApiKeys, Errors}
 import org.apache.kafka.common.requests.JoinGroupRequest
 import org.apache.kafka.common.utils.ProducerIdAndEpoch
 import org.apache.kafka.coordinator.group.GroupCoordinatorConfig
 import org.apache.kafka.coordinator.transaction.TransactionLogConfig
-import org.junit.jupiter.api.Assertions.{assertEquals, assertTrue, fail}
+import org.junit.jupiter.api.Assertions.{assertEquals, assertThrows, assertTrue, fail}
 import org.junit.jupiter.api.extension.ExtendWith
 
 import scala.jdk.CollectionConverters.IterableHasAsScala
@@ -82,99 +83,152 @@ class TxnOffsetCommitRequestTest(cluster:ClusterInstance) extends GroupCoordinat
 
     createTopic(topic, 1)
 
-    def verifyTxnCommitAndFetch(
-      groupId: String,
-      memberId: String,
-      generationId: Int,
-      offset: Long,
-      version: Short,
-    ): Unit = {
-      var producerIdAndEpoch: ProducerIdAndEpoch = null
-      // Wait until ALLOCATE_PRODUCER_ID request finished
-      TestUtils.waitUntilTrue(() =>
-        try {
-          producerIdAndEpoch = initProducerId(
-            transactionalId = transactionalId,
-            producerIdAndEpoch = ProducerIdAndEpoch.NONE,
-            expectedError = Errors.NONE
-          )
-          true
-        } catch {
-          case _: Throwable => false
-        }, "initProducerId request failed"
-      )
-
-      addOffsetsToTxn(
-        groupId = groupId,
-        producerId = producerIdAndEpoch.producerId,
-        producerEpoch = producerIdAndEpoch.epoch,
-        transactionalId = transactionalId
-      )
-
-      commitTxnOffset(
-        groupId = groupId,
-        memberId = memberId,
-        generationId = generationId,
-        producerId = producerIdAndEpoch.producerId,
-        producerEpoch = producerIdAndEpoch.epoch,
-        transactionalId = transactionalId,
-        topic = topic,
-        partition = partition,
-        offset = offset,
-        expectedError = Errors.NONE,
-        version = version
-      )
-
-      endTxn(
-        producerId = producerIdAndEpoch.producerId,
-        producerEpoch = producerIdAndEpoch.epoch,
-        transactionalId = transactionalId,
-        isTransactionV2Enabled = false,
-        committed = true,
-        expectedError = Errors.NONE
-      )
-
-      TestUtils.waitUntilTrue(() =>
-        try {
-          val fetchOffsetsResp = fetchOffsets(
-            groups = Map(groupId -> List(new TopicPartition(topic, partition))),
-            requireStable = true,
-            version = ApiKeys.OFFSET_FETCH.latestVersion
-          )
-          val groupIdRecord = fetchOffsetsResp.find(_.groupId == groupId).head
-          val topicRecord = groupIdRecord.topics.asScala.find(_.name == topic).head
-          val partitionRecord = topicRecord.partitions.asScala.find(_.partitionIndex == partition).head
-          assertEquals(offset, partitionRecord.committedOffset)
-          true
-        } catch {
-          case _: Throwable => false
-        }, "txn commit offset validation failed"
-      )
-    }
-
     for (version <- 0 to ApiKeys.TXN_OFFSET_COMMIT.latestVersion(isUnstableApiEnabled)) {
       // Verify that the TXN_OFFSET_COMMIT request is processed correctly when member id is UNKNOWN_MEMBER_ID
       // and generation id is UNKNOWN_GENERATION_ID under all api versions
       verifyTxnCommitAndFetch(
+        topic = topic,
+        partition = partition,
+        transactionalId = transactionalId,
         groupId = groupId,
         memberId = JoinGroupRequest.UNKNOWN_MEMBER_ID,
         generationId = JoinGroupRequest.UNKNOWN_GENERATION_ID,
         offset = 100 + version,
-        version = version.toShort
+        version = version.toShort,
+        expectedTxnCommitError = Errors.NONE
       )
 
-      // Verify that the TXN_OFFSET_COMMIT request is processed correctly when the member ID
-      // and generation ID are known. This validation starts from version 3, as the member ID
-      // must not be empty from version 3 onwards.
       if (version >= 3) {
+        // Verify that the TXN_OFFSET_COMMIT request is processed correctly when the member ID
+        // and generation ID are known. This validation starts from version 3, as the member ID
+        // must not be empty from version 3 onwards.
         verifyTxnCommitAndFetch(
+          topic = topic,
+          partition = partition,
+          transactionalId = transactionalId,
           groupId = groupId,
           memberId = memberId,
           generationId = memberEpoch,
           offset = 200 + version,
-          version = version.toShort
+          version = version.toShort,
+          expectedTxnCommitError = Errors.NONE
+        )
+
+        // Verify TXN_OFFSET_COMMIT request failed with incorrect memberId
+        verifyTxnCommitAndFetch(
+          topic = topic,
+          partition = partition,
+          transactionalId = transactionalId,
+          groupId = groupId,
+          memberId = "non-exist",
+          generationId = memberEpoch,
+          offset = 200 + version,
+          version = version.toShort,
+          expectedTxnCommitError = Errors.UNKNOWN_MEMBER_ID
+        )
+
+        // Verify TXN_OFFSET_COMMIT request failed with incorrect generationId
+        verifyTxnCommitAndFetch(
+          topic = topic,
+          partition = partition,
+          transactionalId = transactionalId,
+          groupId = groupId,
+          memberId = memberId,
+          generationId = 100,
+          offset = 200 + version,
+          version = version.toShort,
+          expectedTxnCommitError = Errors.ILLEGAL_GENERATION
+        )
+      } else {
+        // Verify that the TXN_OFFSET_COMMIT request failed when group metadata is set under version 3.
+        assertThrows(classOf[UnsupportedVersionException], () =>
+          verifyTxnCommitAndFetch(
+            topic = topic,
+            partition = partition,
+            transactionalId = transactionalId,
+            groupId = groupId,
+            memberId = memberId,
+            generationId = memberEpoch,
+            offset = 200 + version,
+            version = version.toShort,
+            expectedTxnCommitError = Errors.NONE
+          )
         )
       }
     }
+  }
+
+  def verifyTxnCommitAndFetch(
+    topic: String,
+    partition: Int,
+    transactionalId: String,
+    groupId: String,
+    memberId: String,
+    generationId: Int,
+    offset: Long,
+    version: Short,
+    expectedTxnCommitError: Errors
+  ): Unit = {
+    var producerIdAndEpoch: ProducerIdAndEpoch = null
+    // Wait until ALLOCATE_PRODUCER_ID request finished
+    TestUtils.waitUntilTrue(() =>
+      try {
+        producerIdAndEpoch = initProducerId(
+          transactionalId = transactionalId,
+          producerIdAndEpoch = ProducerIdAndEpoch.NONE,
+          expectedError = Errors.NONE
+        )
+        true
+      } catch {
+        case _: Throwable => false
+      }, "initProducerId request failed"
+    )
+
+    addOffsetsToTxn(
+      groupId = groupId,
+      producerId = producerIdAndEpoch.producerId,
+      producerEpoch = producerIdAndEpoch.epoch,
+      transactionalId = transactionalId
+    )
+
+    commitTxnOffset(
+      groupId = groupId,
+      memberId = memberId,
+      generationId = generationId,
+      producerId = producerIdAndEpoch.producerId,
+      producerEpoch = producerIdAndEpoch.epoch,
+      transactionalId = transactionalId,
+      topic = topic,
+      partition = partition,
+      offset = offset,
+      expectedError = expectedTxnCommitError,
+      version = version
+    )
+
+    endTxn(
+      producerId = producerIdAndEpoch.producerId,
+      producerEpoch = producerIdAndEpoch.epoch,
+      transactionalId = transactionalId,
+      isTransactionV2Enabled = false,
+      committed = true,
+      expectedError = Errors.NONE
+    )
+
+    TestUtils.waitUntilTrue(() =>
+      try {
+        val fetchOffsetsResp = fetchOffsets(
+          groups = Map(groupId -> List(new TopicPartition(topic, partition))),
+          requireStable = true,
+          version = ApiKeys.OFFSET_FETCH.latestVersion
+        )
+        val groupIdRecord = fetchOffsetsResp.find(_.groupId == groupId).head
+        val topicRecord = groupIdRecord.topics.asScala.find(_.name == topic).head
+        val partitionRecord = topicRecord.partitions.asScala.find(_.partitionIndex == partition).head
+        assertEquals(offset, partitionRecord.committedOffset)
+        true
+      } catch {
+        case _: Throwable => false
+      }, "txn commit offset validation failed"
+    )
   }
 }
