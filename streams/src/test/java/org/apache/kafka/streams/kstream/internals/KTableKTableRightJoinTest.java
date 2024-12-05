20173d4630ed4016c5a1d1dbb8d5ab32d8ea585d
Almog Gavra <almog.gavra@gmail.com>
2024-12-04 20:46:11 -0800

    KAFKA-18026: KIP-1112, migrate table-table joins to use ProcesserSupplier#stores

--- streams/src/test/java/org/apache/kafka/streams/kstream/internals/KTableKTableRightJoinTest.java
+++ streams/src/test/java/org/apache/kafka/streams/kstream/internals/KTableKTableRightJoinTest.java
@@ -47,6 +47,7 @@ public class KTableKTableRightJoinTest {
         final Processor<String, Change<String>, String, Change<Object>> join = new KTableKTableRightJoin<>(
             (KTableImpl<String, String, String>) builder.table("left", Consumed.with(Serdes.String(), Serdes.String())),
             (KTableImpl<String, String, String>) builder.table("right", Consumed.with(Serdes.String(), Serdes.String())),
+            null,
             null
         ).get();
 
