343bb544289453a19993a3282aa59adcf47d7156
Bill Bejeck <bbejeck@apache.org>
2024-12-05 11:05:40 -0500

    Add KIP-1082 (#18044)
    This PR integrates KIP-1082 changes into the KIP-1071. The change centers around clients creating the member id on startup, vs. waiting for the GroupCoordinator to create the member id and return it in the response to initial heartbeat.
    
    The changes were small and included some changes to the GroupMetadataManagerTest streams group related tests.
    I'll add comments in-line regarding the updates. Overall, the impact can be best summarized by stating that now certain actions will now happen one epoch earlier. For example, task assignment would typically happen on epoch 2 (bumping up from 1) since clients waited for the membership id to be returned and the assignment occurred in the subsequent heartbeat request.
    
    For testing, I updated the GroupMetadataManagerTest to get all tests passing.

--- group-coordinator/src/main/java/org/apache/kafka/coordinator/group/GroupMetadataManager.java
+++ group-coordinator/src/main/java/org/apache/kafka/coordinator/group/GroupMetadataManager.java
@@ -1671,12 +1671,10 @@ public class GroupMetadataManager {
     }
 
     /**
-    /**
      * Validates the request.
      *
      * @param request The request to validate.
-     *
-     * @throws InvalidRequestException if the request is not valid.
+     * @throws InvalidRequestException      if the request is not valid.
      * @throws UnsupportedAssignorException if the assignor is not supported.
      */
     private void throwIfStreamsGroupHeartbeatRequestIsInvalid(
@@ -1685,10 +1683,10 @@ public class GroupMetadataManager {
         throwIfEmptyString(request.groupId(), "GroupId can't be empty.");
         throwIfEmptyString(request.instanceId(), "InstanceId can't be empty.");
         throwIfEmptyString(request.rackId(), "RackId can't be empty.");
+        throwIfEmptyString(request.memberId(), "MemberId can't be empty.");
 
-        if (request.memberEpoch() > 0 || request.memberEpoch() == LEAVE_GROUP_MEMBER_EPOCH) {
-            throwIfEmptyString(request.memberId(), "MemberId can't be empty.");
-        } else if (request.memberEpoch() == 0) {
+
+        if (request.memberEpoch() == 0) {
             if (request.rebalanceTimeoutMs() == -1) {
                 throw new InvalidRequestException("RebalanceTimeoutMs must be provided in first request.");
             }
@@ -2256,7 +2254,6 @@ public class GroupMetadataManager {
         throwIfStreamsGroupIsFull(group, memberId);
 
         // Get or create the member.
-        if (memberId.isEmpty()) memberId = Uuid.randomUuid().toString();
         StreamsGroupMember member;
         StreamsGroupMember.Builder updatedMemberBuilder;
         boolean staticMemberReplaced = false;
